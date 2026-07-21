bcd5338ee7c2086413ffb67fb9cca2d3ad02f7ab
Swaroop-Bobba <swaroop.bobba@techwish.com>
2026-07-21 11:44:31 +0530

    21 July

--- force-app/main/default/lwc/visitOwnerEditor/visitOwnerEditor.js
+++
@@ -1,4 +0,0 @@
-import { LightningElement, api } from 'lwc';
-export default class VisitOwnerEditor extends LightningElement {
-    @api value;
-}
--- force-app/main/default/lwc/visitStatusEditor/visitStatusEditor.js
+++
@@ -1,4 +0,0 @@
-import { LightningElement, api } from 'lwc';
-export default class VisitStatusEditor extends LightningElement {
-    @api value;
-}
